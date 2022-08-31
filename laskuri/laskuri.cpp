44dce4cf100be6ad648cee075350cabda4aa6bb2
oskari.silvennoinen <oskari.silvennoinen@LAPTOP-3B4TD24B>
2022-08-31 12:35:52 +0300

    Add project files.

---
+++ laskuri/laskuri.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "finnish");
+
+	while (true) {
+		cout << "Laskuri by seikkailija007\n" << endl;
+		cout << "[0] Poistu" << endl;
+		cout << "[1] Kuinka monta prosenttia" << endl;
+		cout << "[2] Muutos" << endl;
+		cout << "[3] Vertailu" << endl;
+		cout << "[4] Prosenttiarvo" << endl;
+		cout << "Tila: ";
+		char mode;
+		cin >> mode;
+		if (mode == '0') {
+			system("cls");
+			break;
+		}
+		else if (mode == '1') {
+			system("cls");
+			while (true) {
+				int total;
+				int other;
+				cout << "Yhteensä: ";
+				cin >> total;
+				cout << "vähemmistö: ";
+				cin >> other;
+				float total1 = static_cast<float>(total);
+				float other1 = static_cast<float>(other);
+				float answer = other1 / total1 * 100.0;
+				int ans = static_cast<int>(answer);
+				if (ans < 0) {
+					ans = abs(ans);
+				}
+				cout << "Vastaus on: " << ans << "%" << endl;
+				cout << "Haluatko laskea uudestaan [y] kyllä [n] ei: ";
+				char tum;
+				cin >> tum;
+				if (tum == 'y') {
+					system("cls");
+				}
+				else if (tum == 'n') {
+					system("cls");
+					break;
+				}
+			}
+		}
+		else if (mode == '2') {
+			system("cls");
+			while (true) {
+				cout << "Alkuperäinen arvo: ";
+				int original;
+				cin >> original;
+				cout << "Uusi arvo: ";
+				int after;
+				cin >> after;
+				float orig = static_cast<float>(original);
+				float aft = static_cast<float>(after);
+				float change = orig - aft;
+				float answer = change / orig * 100.0;
+				int ans = static_cast<int>(answer);
+				if (ans < 0) {
+					ans = abs(ans);
+				}
+				cout << "Vastaus on " << ans << "%" << endl;
+				cout << "Haluatko laskea uudestaan [y] kyllä [n] ei: ";
+				char tum;
+				cin >> tum;
+				if (tum == 'y') {
+					system("cls");
+				}
+				else {
+					system("cls");
+					break;
+				}
+			}
+		}
+		else if (mode == '3') {
+			system("cls");
+			while (true) {
+				cout << "Vertaava arvo: ";
+				float first;
+				cin >> first;
+				cout << "Verrattava arvo: ";
+				float second;
+				cin >> second;
+				float diff = first - second;
+				float answer = diff / second * 100;
+				int ans = static_cast<int>(answer);
+				if (ans < 0) {
+					ans = abs(ans);
+				}
+				cout << "vastaus on " << ans << "%" << endl;
+				cout << "Haluatko laskea uudestaan [y] kyllä [n] ei: ";
+				char tum;
+				cin >> tum;
+				if (tum == 'y') {
+					system("cls");
+				}
+				else {
+					system("cls");
+					break;
+				}
+			}
+		}
+		else if (mode == '4') {
+			system("cls");
+			while (true) {
+				cout << "Prosentit: ";
+				float percentage;
+				cin >> percentage;
+				cout << "Arvo: ";
+				float value;
+				cin >> value;
+				float answer = percentage / 100 * value;
+				int ans = static_cast<int>(answer);
+				if (ans < 0) {
+					ans = abs(ans);
+				}
+				cout << "Vastaus on: " << ans << endl;
+				cout << "Haluatko laskea uudestaan [y] kyllä [n] ei: ";
+				char tum;
+				cin >> tum;
+				if (tum == 'y') {
+					system("cls");
+				}
+				else {
+					system("cls");
+					break;
+				}
+			}
+		}
+		else {
+			system("cls");
+		}
+	}
+
+	return 0;
+}
